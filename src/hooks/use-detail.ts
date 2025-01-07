4c82b48f5ddf7a97811c4d3a191369823811e9f6
657 <kuyermqi@outlook.com>
2025-01-07 09:43:56 +0800

    refactor: real http request

--- src/hooks/use-detail.ts
+++ src/hooks/use-detail.ts
@@ -3,12 +3,12 @@ import type { Product } from '../models/product'
 import { fetchProductDetail } from '../services/product'
 
 export const useDetail = (product: Product | null) => {
-  const { /* error,*/ data, isPending } = useQuery({
-    queryKey: ['detail', `product-${product?.id}-detail`],
+  const { error, data, isPending } = useQuery({
+    queryKey: [`product-${product?.id}-detail`],
     queryFn: () => {
       return product === null ? { description: '' } : fetchProductDetail(product.id)
     },
   })
 
-  return { productWithDes: { ...product, ...data }, isPending }
+  return { productWithDes: data, isPending, error }
 }
--- src/services/product.ts
+++ src/services/product.ts
@@ -1,36 +1,37 @@
-import { randomInt } from '../utils/random'
-import { Product } from '../models/product'
+// import { randomInt } from '../utils/random'
+// import { Product } from '../models/product'
 
-// const HOST = 'https://fakestoreapi.com'
+const HOST = 'https://fakestoreapi.com'
 
-// const baseFetch = (api: string, init?: RequestInit) => {
-//   return fetch(`${HOST}${api}`, init)
-// }
+const baseFetch = async (api: string, init?: RequestInit) => {
+  const response = await fetch(`${HOST}${api}`, init)
+  return response.json()
+}
 
-const categories = ['Electronics', 'Fashion & Apparel', 'Home & Kitchen', 'Sports & Outdoors', 'Health & Personal Care', 'Toys & Games', 'Automotive', 'Books & Media', 'Beauty & Skincare', 'Office Supplies']
+// const categories = ['Electronics', 'Fashion & Apparel', 'Home & Kitchen', 'Sports & Outdoors', 'Health & Personal Care', 'Toys & Games', 'Automotive', 'Books & Media', 'Beauty & Skincare', 'Office Supplies']
 
 export const fetchProducts = () => {
-  // return baseFetch(`/products`)
-  console.debug(`fetch product`)
-  const mockData = Array.from({ length: 64 }).map((_, i) => ({
-    id: `${i}`,
-    title: `Product_${i}`,
-    image: `/images/${i % 6 + 1}.avif`,
-    category: [categories[i % categories.length]],
-    price: 100 + (i % 5) * 100,
-  }))
-  return new Promise<Product[]>(resolve => {
-    setTimeout(() => resolve(mockData), randomInt(1200, 300))
-  })
+  return baseFetch(`/products`)
+  // console.debug(`fetch product`)
+  // const mockData = Array.from({ length: 64 }).map((_, i) => ({
+  //   id: `${i}`,
+  //   title: `Product_${i}`,
+  //   image: `/images/${i % 6 + 1}.avif`,
+  //   category: [categories[i % categories.length]],
+  //   price: 100 + (i % 5) * 100,
+  // }))
+  // return new Promise<Product[]>(resolve => {
+  //   setTimeout(() => resolve(mockData), randomInt(1200, 300))
+  // })
 }
 
 export const fetchProductDetail = (id: string) => {
-  // return baseFetch(`/products/${id}`)
-  console.debug(`fetch product detail [${id}]`)
-  return new Promise<{ description: string }>(resolve => {
-    setTimeout(
-      () => resolve({ description: 'Our product combines modern technology with user demands, designed to enhance everyday experiences. It features innovative functionality and versatile adaptability, optimized for various scenarios and personal preferences. Whether for work, entertainment, or daily life, this product delivers unexpected convenience and enjoyment. Choose it to embark on a journey of endless possibilities.' }),
-      randomInt(1200, 300),
-    )
-  })
+  return baseFetch(`/products/${id}`)
+  // console.debug(`fetch product detail [${id}]`)
+  // return new Promise<{ description: string }>(resolve => {
+  //   setTimeout(
+  //     () => resolve({ description: 'Our product combines modern technology with user demands, designed to enhance everyday experiences. It features innovative functionality and versatile adaptability, optimized for various scenarios and personal preferences. Whether for work, entertainment, or daily life, this product delivers unexpected convenience and enjoyment. Choose it to embark on a journey of endless possibilities.' }),
+  //     randomInt(1200, 300),
+  //   )
+  // })
 }
